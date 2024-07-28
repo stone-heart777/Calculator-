0a4fe3035708b3f0e893516677f9565c935907a8
Mr.Stone <snowismydestiny@gmail.com>
2024-07-29 00:17:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let currentDisplay='';
+    document.querySelector('#display').value=currentDisplay;
+
+
+
+console.log('My first Project...');
+console.log('Believe Me When i Say!!!');
+console.log('The Journey has Just Begun 😏');
